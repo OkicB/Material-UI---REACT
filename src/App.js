da7994230bbc0205a57c3aaf1b66cd6a2c0f19c6
OkicB <55504119+OkicB@users.noreply.github.com>
2021-04-11 20:38:37 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { orange, green } from '@material-ui/core/colors';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    }
+  },
+})
+
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root} >Test styled button</Button>
+}
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return(
+    <FormControlLabel 
+      control={<Checkbox
+        checked={checked}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          'aria-label': 'secondary checkbox'
+        }}
+      />}
+      label="Testing Checkbox"
+    />
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary">
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                  MUI Themeing
+                </Typography>
+                <Button>
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+
+            <Typography variant="h2" component="div">
+              Welcome to Bekir's app
+            </Typography>
+            <Typography variant="subtitle1">
+              Learn to use Material UI
+            </Typography>
+            <ButtonStyled />
+
+            <Grid container spacing={4} justify="center">
+              <Grid item xs={12} sm={6}>
+                <Paper style={{ height: 75, width:'100%',  }} />
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width:'100%',  }} />
+              </Grid>
+              <Grid item xs={3} lg={12}>
+                <Paper style={{ height: 75, width:'100%',  }} />
+              </Grid>
+            </Grid>
+
+            <CheckboxExample />
+            <ButtonGroup variant="contained" color="primary">
+              <Button 
+              startIcon={<SaveIcon />}>
+                Save
+              </Button>
+              <Button 
+              startIcon={<DeleteIcon />}>
+                Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+//variant - kako će da izgleda (filled, outlined...)
+//startIcon - na početku a end na kraju objekta
+//wrap(omotana aktivnost) - inputProps 
+//icon={<DeleteIcon/>} checkedIcon={<SaveIcon/>} - kada je checked prikaže save ikonu a unchecked delete
